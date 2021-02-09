a9beaf39f42028a43363ca37c5ebe60c4c6c1f71
DilankaWanigasekara <dilanka>
2021-02-09 14:37:55 +0530

    added database configurations

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAMES', 'driverdroid'),
+        'USER': os.environ.get('DATABASE_USER', 'dilanka'),
+        'PASSWORD': os.environ.get('DARABASE_PWD', 'root'),
+        'HOST': os.environ.get('DATABASE_HOST', 'localhost'),
+        'PORT': os.environ.get('DATABASE_PORT', '3306')
     }
 }
 
