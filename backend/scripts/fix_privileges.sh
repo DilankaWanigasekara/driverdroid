64bc0753be9bb3ae1b36ba0f29b2028eee577396
DilankaWanigasekara <dilanka>
2021-04-19 13:36:57 +0530

    deployment root user updated

--- backend/scripts/fix_privileges.sh
+++ backend/scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user2/server/* jar
-chmod +x /home/ec2-user2/server/server_start.sh
-chmod +x /home/ec2-user2/server/server_stop.sh
+chmod +x /home/ec2-user/server/* jar
+chmod +x /home/ec2-user/server/server_start.sh
+chmod +x /home/ec2-user/server/server_stop.sh
