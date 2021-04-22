d2e755ee041787c7de42c94618a99b911cfba995
Rusiru Wijethilake <rusirumc@gmail.com>
2021-04-22 23:35:14 +0530

    Data Science Branch Cleaned

--- backend/scripts/fix_privileges.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
-
--- backend/src/main/java/com/backend/main/repository/FormRepo.java
+++
@@ -1,9 +0,0 @@
-package com.backend.main.repository;
-
-import com.backend.main.models.ContactForm;
-import com.backend.main.models.Device;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FormRepo extends CrudRepository<ContactForm, String> {
-
-}
